3ee5d46b8a685f93b63aa94482cd1f95bc23a27c
KevinMartinezC <ceronk003@gmail.com>
2025-11-20 09:01:44 -0600

    feat: add scrolling

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,14 +2,27 @@ import 'package:chat_app/domain/entities/message.dart';
 import 'package:flutter/material.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: "Hi, my friend", fromWho: FromWho.mine),
     Message(text: "How are you?", fromWho: FromWho.hers),
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messageList.add(newMessage);
     notifyListeners();
+    moveScrollToBottm();
+  }
+
+  Future<void> moveScrollToBottm() async {
+    await  Future.delayed(const Duration(microseconds: 100));
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: Duration(microseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
