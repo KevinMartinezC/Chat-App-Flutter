6464a1ea75a4918d30bab720a28635fd1efca772
KevinMartinezC <ceronk003@gmail.com>
2025-11-19 18:48:18 -0600

    feat: add TextEditingController and focusNode

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MessageFieldBox extends StatelessWidget {
   const MessageFieldBox({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+  
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40),
     );
 
+
     return TextFormField(
+      controller: textController,
+      focusNode: focusNode,
       decoration: InputDecoration(
+        hintText: 'End your message with a "?"',
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           onPressed: () {
-            print("Bottom click");
+            final textValue = textController.value.text;
+            textController.clear();
           },
           icon: Icon(Icons.send_outlined),
         ),
       ),
-      onFieldSubmitted: (value) => {
-        print("send message $value")
+      onFieldSubmitted: (value) {
+        textController.clear();
+        focusNode.requestFocus();
       },
       onChanged: (value) => {
         print("on changed $value")
+      },
+      onTapOutside: (event) {
+        focusNode.unfocus();
       },
     );
   }
