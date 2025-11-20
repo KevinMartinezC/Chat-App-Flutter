2723cec3ab4382aa1fafd91ed574ca245da94664
KevinMartinezC <ceronk003@gmail.com>
2025-11-20 10:21:09 -0600

    feat: implement get yes or not answer

---
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,11 @@
+import 'package:chat_app/domain/entities/message.dart';
+import 'package:dio/dio.dart';
+
+class GetYesNoAnswer {
+  final _dio = Dio();
+
+  Future<Message> getAnswer() async {
+    final response = await _dio.get("https://yesno.wtf/api");
+    throw UnimplementedError();
+  }
+}
--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
+import 'package:chat_app/config/helpers/get_yes_no_answer.dart';
 import 'package:chat_app/domain/entities/message.dart';
 import 'package:flutter/material.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: "Hi, my friend", fromWho: FromWho.mine),
@@ -13,12 +15,19 @@ class ChatProvider extends ChangeNotifier {
     if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messageList.add(newMessage);
+    if (text.endsWith("?")) {
+      herReply();
+    }
     notifyListeners();
     moveScrollToBottm();
   }
 
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+  }
+
   Future<void> moveScrollToBottm() async {
-    await  Future.delayed(const Duration(microseconds: 100));
+    await Future.delayed(const Duration(microseconds: 100));
     chatScrollController.animateTo(
       chatScrollController.position.maxScrollExtent,
       duration: Duration(microseconds: 300),
